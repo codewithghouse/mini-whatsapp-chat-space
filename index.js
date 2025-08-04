adac08ae789f16072aaaf70f5ab88a9ebe5c2a58
codewithghouse <personalghouse@gmail.com>
2025-08-05 03:33:18 +0530

    utils files added

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ app.use(express.urlencoded({extended:true}));
 // method override
 // for patch, delete and put request
 const methodOverride = require('method-override');
+
 app.use(methodOverride('_method'));
+const ExpressError = require("../utils/ExpressError.js");
+const asyncWrap = require("../utils/AsyncWrap.js");
 
 
 // connecting to mongodb
@@ -28,21 +31,48 @@ let connect_db= async ()=>{
 
     } catch (e) {
        console.log(e.message,"some error in db");
+       process.exit(1);
        
     }
 }
 connect_db();
 app.get("/",((req,res)=>{
-    res.send(" hello i am root");
+res.redirect("/chats")
 }))
 // Index Route
-app.get("/chats",(async (req,res)=>{
+app.get("/chats",(async (req,res,next)=>{
     // console.log("hello from chats");
-    //
+   try{
+     //
     let allchats= await Chat.find();
     // console.log(allchats);
    res.render("index.ejs",{allchats});
+   }catch(err){
+    next(err);
+   }
 }))
+// search route
+app.get("/chats/search", async (req, res, next) => {
+  try {
+    let { from } = req.query;
+    console.log("Searching chats for user:", from);
+
+    if (!from) {
+      return next(new ExpressError(404, "User not found"));
+    }
+   
+    let results = await Chat.find({
+      $or: [{ from: from }, { to: from }]
+    });
+    // if(results.length===0){
+    //   return next( new ExpressError(404,"chat not found"));
+    // }
+
+    res.render("searchResults.ejs", { results, searchUser: from });
+  } catch (e) {
+    next(e);
+  }
+});
 // New Route
 app.get("/chats/new",async(req,res)=>{
     // console.log("add new user");
@@ -50,10 +80,11 @@ app.get("/chats/new",async(req,res)=>{
     res.render("new.ejs");
 })
 // create route
-app.post("/chats",((req,res)=>{
+app.post("/chats",(async(req,res,next)=>{
 
 // form se data ku lenge ab
-let{ from,to,message}= req.body;
+try {
+  let{ from,to,message}= req.body;
 // jo data aya form se uske liye new object banare 
    let newchat= new Chat({
     from: from,
@@ -63,15 +94,34 @@ let{ from,to,message}= req.body;
    });
    // data jo ara hamare pass ham usku date lagake send karre databse me 
 
-   newchat.save().then(res =>{
-    console.log(" the user addedd successfully");
-   }).catch(e =>{
-    console.log(e.message);
-   })
+    await newchat.save();
    res.redirect("/chats");
-}))
-// Edit route
-app.get("/chats/:id/edit",(async(req,res)=>{
+  
+} catch (err) {
+  next(err);
+}
+}));
+// function asyncWrap(fn){
+//     return function (req,res,next){
+//         fn(req,res,next).catch((err)=>{
+//             console.log(err.message);
+//             next(err);
+//         })
+//     }
+
+// }
+// Show
+app.get("/chats/:id", asyncWrap((async (req, res, next) => {
+    const { id } = req.params;
+  const chat = await Chat.findById(id);
+  if (!chat){ 
+    return next(new ExpressError(404, "Chat not found"))
+  };
+  res.render("show.ejs", { chat });
+    //agar koe bhi error ayega mongoose ka ya phir aysnc function ka to error dono way me call honge and epress bydefualt error thorew karega
+})));
+//Edit route
+app.get("/chats/:id/edit",(async(req,res,next)=>{
 
   try {
     let {id}= req.params;
@@ -82,13 +132,14 @@ app.get("/chats/:id/edit",(async(req,res)=>{
      // res.send("edit route working here ")
     
   } catch (e) {
-    console.log(e.message)
+    next(e);
     
   }
 }));
 // Update route
-app.patch("/chats/:id",(async(req,res)=>{
-    console.log("the patch is working");
+app.patch("/chats/:id",(async(req,res,next)=>{
+   try{
+     console.log("the patch is working");
     let { id}= req.params;
     let{message:message}= req.body;
     console.log(message)
@@ -98,9 +149,12 @@ app.patch("/chats/:id",(async(req,res)=>{
     console.log(updatechat)
     // res.send("the patch is wroking here ")
     res.redirect("/chats");
+   }catch(e){
+    next(e);
+   }
 }))
 //Delete Ejs Form
-app.get("/chats/:id/delete",(async(req,res)=>{
+app.get("/chats/:id/delete",(async(req,res,next)=>{
     try {
         let{ id}= req.params;
         let chat= await Chat.findById(id);
@@ -108,11 +162,12 @@ app.get("/chats/:id/delete",(async(req,res)=>{
         
     } catch (e) {
         console.log("some error in deleting the user",e.message);
+        next(e);
         
     }
 }))
 // delete route
-app.delete("/chats/:id",(async(req,res)=>{
+app.delete("/chats/:id",(async(req,res,next)=>{
    try {
     let{ id}= req.params;
     let delChat= await Chat.findByIdAndDelete(id);
@@ -120,26 +175,17 @@ app.delete("/chats/:id",(async(req,res)=>{
     res.redirect("/chats");
    } catch (e) {
     console.log(e.message)
+    next(e);
     
    }
 }))
-// search route
-app.get("/chats/search", async (req, res) => {
-    try {
-      let { from } = req.query;
-      console.log("Searching chats for user:", from);
-  
-      let results = await Chat.find({
-        $or: [{ from: from }, { to: from }]
-      });
-  
-      res.render("searchResults.ejs", { results, searchUser: from });
-    } catch (e) {
-      console.log(e.message);
-      res.send("Some error occurred.");
-    }
-  });
-  
+
+
+//defining the custom error handler middlware 
+app.use((err,req,res,next)=>{
+  let {status=500,message="something has brokern"}= err;
+  res.status(status).send(message);
+})
 app.listen(port,()=>{
     console.log(`the app is listening on the port ${port}`);
 })
